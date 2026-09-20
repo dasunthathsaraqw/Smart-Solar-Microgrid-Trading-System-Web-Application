43909dbef43503e10eea8f7a4120b99ada7e3135
thathsarahema18@gmail.com <thathsarahema18@gmail.com>
2026-09-20 20:30:22 +0530

    Add user status badge

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,42 @@ import CreateAccount from "./pages/CreateAccount";
 import DashboardBackoffice from "./pages/DashboardBackoffice";
 import DashboardOperator from "./pages/DashboardOperator";
 import RequireAuth from "./components/RequireAuth";
+import { ConfirmProvider } from "./components/ConfirmProvider";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/create-account"
-          element={
-            <RequireAuth allowedRoles={["Backoffice"]}>
-              <CreateAccount />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/dashboard/backoffice"
-          element={
-            <RequireAuth allowedRoles={["Backoffice"]}>
-              <DashboardBackoffice />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/dashboard/operator"
-          element={
-            <RequireAuth allowedRoles={["GridOperator"]}>
-              <DashboardOperator />
-            </RequireAuth>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <ConfirmProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/create-account"
+            element={
+              <RequireAuth allowedRoles={["Backoffice"]}>
+                <CreateAccount />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/dashboard/backoffice"
+            element={
+              <RequireAuth allowedRoles={["Backoffice"]}>
+                <DashboardBackoffice />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/dashboard/operator"
+            element={
+              <RequireAuth allowedRoles={["GridOperator"]}>
+                <DashboardOperator />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ConfirmProvider>
   );
 }
 
